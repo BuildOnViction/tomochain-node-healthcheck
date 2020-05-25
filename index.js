69f20e5a17dc87c91499e0b4f08fdafbb58fbe90
NguyenNguyen <nguyenbk92@gmail.com>
2020-05-25 10:49:18 +0700

    tomochain node healthcheck

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env')
+})
+const Web3 = require("web3")
+const web3 = new Web3(new Web3.providers.HttpProvider("https://" + process.env.RPC_ENDPOINT))
+const localWeb3 = new Web3(new Web3.providers.IpcProvider(process.env.LOCAL_IPC_PATH))
+const noti_bot = require('noti_bot')
+const notifyTelegram = noti_bot.telegram
+const notifySlack = noti_bot.slack
+const THRESHOLD = parseFloat(process.env.DELAY_THRESHOLD)
+
+const main = async () => {
+	let latestBlockNumber = await web3.eth.getBlockNumber()
+	let localLatestBlockNumber = await localWeb3.eth.getBlockNumber()
+	if (localLatestBlockNumber < latestBlockNumber - THRESHOLD) {
+		let coinbase = await localWeb3.eth.getCoinbase()
+	        let msg = "This node coinbase = " + coinbase + " is slow. RPCCurrentBlock="+ latestBlockNumber + " YourCurrentBlock="+localLatestBlockNumber 
+        notifyTelegram(msg, process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT)
+        notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_ICON)
+
+	}
+}
+main()
+
